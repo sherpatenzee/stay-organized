01f42a0a2c70c8785b16345aebc7e841d76814b5
sherpatenzee <sherpatenzee09@gmail.com>
2022-12-14 14:30:20 -0500

    complete page styling

--- script/todo.js
+++ script/todo.js
@@ -4,7 +4,7 @@ window.onload = function () {
 
   let userSelect = document.getElementById("userSelect");
   userSelect.onchange = userDropdownOnChange;
-  
+
   populateUserDropdown();
 }
 
@@ -18,45 +18,50 @@ function populateUserDropdown() {
   defaultOption.textContent = "Please select a user!";
   userSelect.appendChild(defaultOption);
 
-  
+
   fetch("http://localhost:8083/api/users")
     .then(response => response.json())
     .then(data => {
-      
+
       for (let user of data) {
-  
+
         let newOption = document.createElement("option");
         newOption.textContent = user.name;
         newOption.value = user.id;
         userSelect.appendChild(newOption);
-    
+
       }
     });
- 
-  
+
+
 
 }
 
 
-function userDropdownOnChange(){
+function userDropdownOnChange() {
 
-  let display =  document.getElementById("displayUser")
+  let display = document.getElementById("displayUser")
   let userSelect = document.getElementById("userSelect").value
-  
+
 
 
   fetch("http://localhost:8083/api/todos")
     .then(response => response.json())
     .then(data => {
-      
-      for (let user of data) {
-  
-        if( userSelect == user.id){
-          display.innerHTML = "<span style='color: Red ; '> Todo : </span>" + user.description;
 
+      for (let user of data) {
+
+        if (userSelect == user.id) {
+          display.innerHTML = "<span style='color: Red ; '> Catergory: </span>" + user.category + "<br />" + "<span style='color: Red ; '> Description : </span>" + user.description + "<br />" + "<span style='color: Red ; '> Deadline : </span>" + user.deadline + "<br />" + "<span style='color: Red ; '> Priority : </span>" + user.priority + "<br />" + "<span style='color: Red ; '> Completed : </span>" + user.completed;
+
+        }else if (userSelect == ""){
+          display.innerHTML = "";
         }
       }
     });
- 
+
+    
+
 
 }
+
